ffaec005b819776414595cf3b47199338ba371c0
Rajnish Dargan <rajnish.code30@gmail.com>
2024-03-13 16:42:15 +0530

    linux image change

--- projects/quml-library/karma.conf.js
+++ projects/quml-library/karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
+      require('karma-mocha-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -16,21 +16,44 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
+      dir: require('path').join(__dirname, 'coverage'),
       subdir: '.',
-      reporters:[
-        {type: 'text-summary'},
-        {type: 'html'},
-        {type: 'lcovonly'}
-      ],
-      fixWebpackSourcePaths: true
+      reporters: [
+        { type: 'text-summary' },
+        { type: 'html' },
+        { type: 'lcovonly' }
+      ]
     },
-    reporters: ['progress', 'kjhtml'],
+
+    reporters: ['mocha'],
+    mochaReporter: {
+      symbols: {
+        success: '+',
+        info: '#',
+        warning: '!',
+        error: 'x'
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+          '--proxy-server=direct://',
+          '--proxy-bypass-list=*',
+          '--max_old_space_size=4096'
+        ]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
