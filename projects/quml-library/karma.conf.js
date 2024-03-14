e654ecdebb783a6863e42fbc060c3cacb6701cbf
Rajnish Dargan <rajnish.code30@gmail.com>
2024-03-14 10:54:10 +0530

    Fix Image Deprecations

--- projects/quml-library/karma.conf.js
+++ projects/quml-library/karma.conf.js
@@ -1,9 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const argv = require('minimist')(process.argv.slice(2))
-const tags = (argv.tags !== true) && argv.tags
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -11,58 +8,36 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-mocha-reporter'),
+      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    browserNoActivityTimeout: 100000,
     client: {
-      jasmine: {
-        random: false,
-        timeoutInterval: 50000
-      },
-      args: [tags],
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
+      dir: require('path').join(__dirname, '../../coverage'),
       subdir: '.',
-      reporters: [
-        { type: 'text-summary' },
-        { type: 'html' },
-        { type: 'lcovonly' }
-      ]
+      reporters:[
+        {type: 'text-summary'},
+        {type: 'html'},
+        {type: 'lcovonly'}
+      ],
+      fixWebpackSourcePaths: true
     },
-
-    reporters: ['mocha'],
-    mochaReporter: {
-      symbols: {
-        success: '+',
-        info: '#',
-        warning: '!',
-        error: 'x'
-      }
-    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
     customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          // Without a remote debugging port, Google Chrome exits immediately.
-          '--remote-debugging-port=9222',
-          '--proxy-server=direct://',
-          '--proxy-bypass-list=*',
-          '--max_old_space_size=4096'
-        ]
-      }
+        ChromeHeadlessCI: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox']
+        }
     },
-    singleRun: false
-  })
-}
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
