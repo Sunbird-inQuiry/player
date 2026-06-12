700baad707b167ccf3b4a77c19ca0410ac57df5b
Deeksha Deepak <deekshad1502@gmail.com>
2026-06-12 11:25:52 +0530

    fix: update API endpoint paths for question set hierarchy and question set read

--- projects/quml-demo-app/src/app/app.constant.ts
+++ projects/quml-demo-app/src/app/app.constant.ts
@@ -1,6 +1,6 @@
 export const ApiEndPoints = {
   getContent: '/api/content/v1/read/',
-  getQuestionSetHierarchy: '/questionset/v2/hierarchy/',
-  questionSetRead: '/questionset/v2/read/',
+  getQuestionSetHierarchy: '/learner/questionset/v2/hierarchy/',
+  questionSetRead: '/api/questionset/v2/read/',
   questionList: '/api/question/v2/list',
 }
